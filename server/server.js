90a79ac1b759707efdfd1e7a26f749265fa76743
Srashti Rai <srashti1909@gmail.com>
2025-05-13 09:53:22 +0530

    Added Webhooks Added

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import connectDB from "./configs/mongodb.js";
+import userRouter from "./routes/userRoutes.js";
 
 // App Config
 const PORT = process.env.PORT || 4000;
@@ -16,5 +17,6 @@ app.use(cors())
 app.get("/", (req, res) => {
   res.send("API Working");
 });
+app.use("/api/user" , userRouter)
 
 app.listen(PORT, () => console.log("Server Running on port " + PORT));
